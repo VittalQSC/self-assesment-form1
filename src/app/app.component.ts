31cb4d3167468ebaa2b5fd824859bba475d70d84
VittalQSC <vitshacov@gmail.com>
2017-11-15 11:25:41 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+const formData = {
+  min: 0,
+  max: 5,
+  step: 1,
+  tickInterval: 1, 
+  fields: [
+    {
+      value: 0,
+      yValue: 'label #1',
+      xValue: null,
+    },
+    {
+      value: 0,
+      yValue: 'label #2',
+      xValue: null,
+    },
+    {
+      value: 0,
+      yValue: 'label #3',
+      xValue: null,
+    },
+  ],
+  xValues: [],
+  yValues: ['label #1', 'label #2', 'label #3'],
+};
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  fields = formData.fields;
+  tickInterval = formData.tickInterval;
+
+  min = formData.min;
+  max = formData.max;
+  step = formData.step;
+  
+  yValues = formData.fields.map(field => field.yValue);
+
+  constructor () {
+    // setInterval(() => {
+    //   console.log('settimeout', this.value);
+    //   this.value = (this.value+1)%this.max;
+    // }, 2000);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material';
+import { DndModule } from 'ng2-dnd';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    MatSliderModule,
+    DndModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
